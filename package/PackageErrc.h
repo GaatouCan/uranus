eb154a632a8b2d8a914d575970fefa7e149127e1
Astria <1752651025@qq.com>
2025-10-21 01:28:47 +0800

    Initial commit

--- package/PackageErrc.h
+++ package/PackageErrc.h
@@ -3,28 +3,28 @@
 
 namespace uranus::network {
     enum class PackageErrc {
-        kOk = 0,
-        kNullMessage = 1,
-        kEncodeLengthError = 2,
-        kPayloadTooLarge = 3,
-        kPayloadLengthError = 4,
-        kDecodeLengthError = 5,
+        kOk                     = 0,
+        kNullMessage            = 1,
+        kEncodeLengthError      = 2,
+        kPayloadTooLarge        = 3,
+        kPayloadLengthError     = 4,
+        kDecodeLengthError      = 5,
     };
 
-    class PackageCategory : public std::error_category {
+    class PackageCategory final : public std::error_category {
     public:
-        const char *name() const noexcept override {
+        [[nodiscard]] const char *name() const noexcept override {
             return "PackageCategory";
         }
 
-        std::string message(int ev) const override {
+        [[nodiscard]] std::string message(int ev) const override {
             switch (static_cast<PackageErrc>(ev)) {
-                case PackageErrc::kOk: return "Success";
-                case PackageErrc::kNullMessage: return "Message pointer is null";
-                case PackageErrc::kEncodeLengthError: return "Encode length error";
-                case PackageErrc::kPayloadTooLarge: return "Payload too large";
-                case PackageErrc::kPayloadLengthError: return "Payload length error";
-                case PackageErrc::kDecodeLengthError: return "Decode length error";
+                case PackageErrc::kOk:                  return "Success";
+                case PackageErrc::kNullMessage:         return "Message pointer is null";
+                case PackageErrc::kEncodeLengthError:   return "Encode length error";
+                case PackageErrc::kPayloadTooLarge:     return "Payload too large";
+                case PackageErrc::kPayloadLengthError:  return "Payload length error";
+                case PackageErrc::kDecodeLengthError:   return "Decode length error";
                 default: return "Unknown error";
             }
         }
@@ -36,12 +36,9 @@ namespace uranus::network {
     };
 
     inline std::error_code MakeErrorCode(PackageErrc err) {
-        return {static_cast<int>(err), uranus::network::PackageCategory::Instance()};
+        return { static_cast<int>(err), PackageCategory::Instance() };
     }
 }
 
-namespace std {
-    template<>
-    struct is_error_code_enum<uranus::network::PackageErrc> : true_type {
-    };
-}
+template<>
+struct std::is_error_code_enum<uranus::network::PackageErrc> : true_type {};
