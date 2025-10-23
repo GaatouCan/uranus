40bf7d9498c04c9db4334af8773f43dc8b4188d6
admin <xxx@xx.xom>
2025-10-23 17:50:38 +0800

    Initial commit

--- core-src/ActorContext.h
+++ core-src/ActorContext.h
@@ -51,23 +51,33 @@ namespace uranus {
         virtual int Start();
         virtual void Stop();
 
+        /// Return a message with custom resource
         [[nodiscard]] virtual Message BuildMessage() = 0;
 
+        /// Send the message to the other actor;
+        /// You should set the .type in msg to tell what kind of actor you want to send
         virtual void Send(int64_t target, const Message &msg) = 0;
 
+        /// Send the message to the service actor by target service' s name
         virtual void SendToService(const std::string &name, const Message &msg) = 0;
 
-        virtual void RemoteCall(int64_t target, Message msg, SessionNode &&node) = 0;
-
+        /// Send a request type message to the target service
+        /// It will be suspended till receive the response
         template<asio::completion_token_for<void(optional<Message>)> CompleteToken>
         auto AsyncCall(int64_t target, const Message &req, CompleteToken &&token);
 
+        /// Other push message to this context;
+        /// Do not call this method in logic code
         void PushMessage(const Message &msg);
 
     protected:
+        /// Make actor' s context pointer point to this context
         void SetUpActor();
 
         [[nodiscard]] bool IsChannelClosed() const;
+
+        /// Implement how to send the request message to the target actor
+        virtual void RemoteCall(int64_t target, Message msg, SessionNode &&node) = 0;
 
         int32_t AllocateSessionID();
         void RecycleSessionID(int32_t id);
@@ -77,13 +87,15 @@ namespace uranus {
 
         awaitable<void> Process();
 
+        /// Handle the message that this context received
+        /// This method will be called in ::Process automatically
         virtual void HandleMessage(const Message &msg) = 0;
 
+        /// Implement how to release the custom resource in message
         virtual void DisposeMessage(const Message &msg) = 0;
 
-        //virtual void OnRequest(const Message &req) = 0;
-        //virtual void OnResponse(const Message &res) = 0;
-
+        /// Clean Up the resource in this context after ::Stop be called
+        /// This method will be called in the end of ::Process automatically
         virtual void CleanUp();
 
     private:
@@ -97,7 +109,10 @@ namespace uranus {
         /// The inner channel to handle the tasks
         unique_ptr<ConcurrentChannel<Message>> channel_;
 
+        /// Allocate and recycle the session ids
         IdentAllocator<int32_t, false> sess_id_alloc_;
+
+        /// The mapping to all the spending request
         std::unordered_map<int32_t, SessionNode> sessions_;
     };
 
