68057a8fac0ffc5b90226955b3d66b938d479e2c
admin <xxx@xx.xom>
2025-10-23 20:09:17 +0800

    Initial commit

--- uranus-src/login/LoginAuth.cpp
+++ uranus-src/login/LoginAuth.cpp
@@ -1,5 +1,11 @@
 #include "LoginAuth.h"
+#include "Package.h"
+#include "../GameWorld.h"
 #include "../gateway/Connection.h"
+#include "../gateway/Gateway.h"
+
+#include <login.pb.h>
+
 
 LoginAuth::LoginAuth(GameServer *ser)
     : ServerModule(ser) {
@@ -9,6 +15,20 @@ LoginAuth::~LoginAuth() {
 }
 
 void LoginAuth::OnPlayerLogin(const shared_ptr<Connection> &conn, Package *pkg) {
+    if (conn == nullptr || pkg == nullptr)
+        return;
+
+    if (pkg->GetPackageID() != 1001)
+        return;
+
+    Login::LoginRequest request;
+    request.ParseFromString(pkg->ToString());
+
+    conn->SetPlayerID(request.player_id());
+
+    if (auto *gateway = GetGameServer()->GetModule<Gateway>()) {
+        gateway->OnPlayerLogin(conn);
+    }
 }
 
 void LoginAuth::OnPlayerLogout(const shared_ptr<Connection> &conn, int64_t pid) {
