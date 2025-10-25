0dc77fa424a5ecfd551c4c558fec0e5d1896fe5d
admin <xxx@xx.xom>
2025-10-25 16:08:49 +0800

    add config.yaml

--- player/route/ProtocolRouter.cpp
+++ player/route/ProtocolRouter.cpp
@@ -3,17 +3,40 @@
 #include "controllers/ChatController.h"
 
 namespace protocol {
-
-    const unordered_map<ControllerType, function<unique_ptr<AbstractController>()>> gControllerCreator = {
-
-        { ControllerType::kChat, []{ return make_unique<ChatController>(); }},
-
-    };
+    unique_ptr<AbstractController> ProtocolRouter::CreateController(const ControllerType type) {
+        switch (type) {
+            case ControllerType::kChat: return make_unique<ChatController>();
+            default: return nullptr;
+        }
+    }
 
     ProtocolRouter::ProtocolRouter(GamePlayer *plr)
         : player_(plr) {
     }
 
     ProtocolRouter::~ProtocolRouter() {
+    }
+
+    void ProtocolRouter::Dispatch(const int id, const Message &msg) {
+        constexpr int min = static_cast<int>(ControllerType::kMinimum) * 100;
+        constexpr int max = static_cast<int>(ControllerType::kMaximum) * 100;
+
+        if (id <= min || id >= max) {
+            //throw std::out_of_range("ProtocolRouter::Register(): id is out of range");
+            return;
+        }
+
+        const auto type = id / 100;
+        const auto iter = controllers_.find(type);
+        if (iter == controllers_.end())
+            return;
+
+        auto *ctrl = iter->second.get();
+
+        const auto func_iter = functors_.find(id);
+        if (func_iter == functors_.end())
+            return;
+
+        std::invoke(func_iter->second, ctrl, player_, msg);
     }
 }
--- player/route/ProtocolRouter.h
+++ player/route/ProtocolRouter.h
@@ -2,12 +2,14 @@
 
 #include "AbstractController.h"
 #include "Message.h"
+#include "ControllerType.h"
+
 
 #include <functional>
 #include <memory>
+#include <stdexcept>
 #include <unordered_map>
 
-#include "../GamePlayer.h"
 
 class GamePlayer;
 
@@ -29,9 +31,39 @@ namespace protocol {
 
         DISABLE_COPY_MOVE(ProtocolRouter)
 
+        template<class T>
+        requires std::is_base_of_v<AbstractController, T>
+        void Register(int id, void(T::*fn)(GamePlayer *, const Message &));
+
+        void Dispatch(int id, const Message &msg);
+
+    private:
+        static unique_ptr<AbstractController> CreateController(ControllerType type);
+
     private:
         GamePlayer *const player_;
         unordered_map<int, unique_ptr<AbstractController>> controllers_;
         unordered_map<int, ProtocolFunctor> functors_;
     };
+
+    template<class T>
+    requires std::is_base_of_v<AbstractController, T>
+    void ProtocolRouter::Register(const int id, void(T::*fn)(GamePlayer *, const Message &)) {
+        constexpr int min = static_cast<int>(ControllerType::kMinimum) * 100;
+        constexpr int max = static_cast<int>(ControllerType::kMaximum) * 100;
+
+        if (id <= min || id >= max) {
+            throw std::out_of_range("ProtocolRouter::Register(): id is out of range");
+        }
+
+        if (const auto type = (id / 100); !controllers_.contains(type)) {
+            controllers_.insert_or_assign(type, CreateController(static_cast<ControllerType>(type)));
+        }
+
+        functors_[id] = [fn](AbstractController *ctrl, GamePlayer *plr, const Message &msg) {
+            if (auto *temp = dynamic_cast<T *>(ctrl)) {
+                (temp->*fn)(plr, msg);
+            }
+        };
+    }
 }
