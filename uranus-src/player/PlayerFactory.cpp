699a3b20cbc861211468c65fa2f23835ec165626
admin <xxx@xx.xom>
2025-12-06 16:23:16 +0800

    Initial commit

--- uranus-src/player/PlayerFactory.cpp
+++ uranus-src/player/PlayerFactory.cpp
@@ -1,12 +1,32 @@
 #include "PlayerFactory.h"
 
-PlayerFactory::PlayerFactory() {
+PlayerFactory::PlayerFactory()
+    : creator_(nullptr),
+      destroyer_(nullptr) {
 }
 
 PlayerFactory::~PlayerFactory() {
 }
 
-PlayerFactory &PlayerFactory::instance() {
-    static PlayerFactory inst;
-    return inst;
+void PlayerFactory::initial() {
+#if defined(_WIN32) || defined(_WIN64)
+    const std::string path = "player.dll";
+#elif defined(__APPLE__)
+    std::string path = "libplayer.dylib"
+#else
+    std::string path = "libplayer.so";
+#endif
+
+    lib_ = SharedLibrary(std::string_view(path));
+
+    if (!lib_.available()) {
+        exit(-1);
+    }
+
+    creator_ = lib_.getSymbol<PlayerCreator>("CreatePlayer");
+    destroyer_ = lib_.getSymbol<PlayerDestroyer>("DestroyPlayer");
+
+    if (creator_ == nullptr || destroyer_ == nullptr) {
+        exit(-2);
+    }
 }
--- uranus-src/player/PlayerFactory.h
+++ uranus-src/player/PlayerFactory.h
@@ -2,15 +2,28 @@
 
 #include <base/noncopy.h>
 #include <base/SharedLibrary.h>
+#include <actor/BasePlayer.h>
 
+
+using uranus::SharedLibrary;
+using uranus::actor::BasePlayer;
 
 class PlayerFactory final {
 
-    PlayerFactory();
+    using PlayerCreator = BasePlayer *(*)();
+    using PlayerDestroyer = void (*)(BasePlayer *);
+
 public:
+    PlayerFactory();
     ~PlayerFactory();
 
     DISABLE_COPY_MOVE(PlayerFactory)
 
-    static PlayerFactory &instance();
+    void initial();
+
+private:
+    SharedLibrary lib_;
+    PlayerCreator creator_;
+    PlayerDestroyer destroyer_;
+
 };
