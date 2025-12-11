5bd7960cdf30975f8b085d70db3bfb04577e1d2b
admin <xxx@xx.xom>
2025-12-11 11:37:35 +0800

    Initial commit

--- actor-src/include/actor/AgentPipelineContext.h
+++ actor-src/include/actor/AgentPipelineContext.h
@@ -4,8 +4,23 @@
 
 namespace uranus::actor {
 
+    class AgentPipeline;
+
     class ACTOR_API AgentPipelineContext final {
 
+        friend class AgentPipeline;
+
+        AgentPipelineContext(AgentPipeline &pipeline, size_t idx);
+
+    public:
+        AgentPipelineContext() = delete;
+        ~AgentPipelineContext();
+
+        [[nodiscard]] AgentPipeline &pipeline() const;
+
+    private:
+        AgentPipeline &pipeline_;
+        const size_t index_;
     };
 
 }
--- actor-src/source/actor/AgentPipelineContext.cpp
+++ actor-src/source/actor/AgentPipelineContext.cpp
@@ -1,5 +1,15 @@
 #include "AgentPipelineContext.h"
 
 namespace uranus::actor {
+    AgentPipelineContext::AgentPipelineContext(AgentPipeline &pipeline, const size_t idx)
+        : pipeline_(pipeline),
+          index_(idx) {
+    }
 
+    AgentPipelineContext::~AgentPipelineContext() {
+    }
+
+    AgentPipeline &AgentPipelineContext::pipeline() const {
+        return pipeline_;
+    }
 }
