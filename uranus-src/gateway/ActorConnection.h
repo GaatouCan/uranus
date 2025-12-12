285ae9774b974e0ea3cd749394ec4d4bb58480a1
astria <1752651025@qq.com>
2025-12-13 01:50:55 +0800

    Initial commit

---
+++ uranus-src/gateway/ActorConnection.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <network/Connection.h>
+#include <actor/PackageCodec.h>
+
+using uranus::network::ConnectionImpl;
+using uranus::actor::PackageCodec;
+
+
+class ActorConnection {
+};
