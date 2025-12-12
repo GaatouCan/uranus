2337a4d2fab17988de639f1266b5dfdc328d7152
astria <1752651025@qq.com>
2025-12-13 01:52:29 +0800

    Initial commit

--- uranus-src/gateway/ActorConnection.h
+++ uranus-src/gateway/ActorConnection.h
@@ -5,7 +5,13 @@
 
 using uranus::network::ConnectionImpl;
 using uranus::actor::PackageCodec;
+using uranus::network::ServerBootstrap;
+using uranus::TcpSocket;
 
 
-class ActorConnection {
+class ActorConnection : public ConnectionImpl<PackageCodec> {
+
+public:
+    ActorConnection(ServerBootstrap &server, TcpSocket &&socket);
+    ~ActorConnection() override;
 };
