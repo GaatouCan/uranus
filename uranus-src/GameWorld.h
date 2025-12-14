9918e4d8caa094a7bae30a61144259cb2bf43a73
Astria <1752651025@qq.com>
2025-12-14 19:09:01 +0800

    Initial commit

--- uranus-src/GameWorld.h
+++ uranus-src/GameWorld.h
@@ -1,4 +1,21 @@
 #pragma once
 
+#include <base/SingleIOContextPool.h>
+#include <actor/ServerModule.h>
+
+using uranus::SingleIOContextPool;
+
 class GameWorld final {
+
+public:
+    GameWorld();
+    ~GameWorld();
+
+    DISABLE_COPY_MOVE(GameWorld)
+
+private:
+    asio::io_context ctx_;
+    asio::executor_work_guard<asio::io_context::executor_type> guard_;
+
+    SingleIOContextPool pool_;
 };
