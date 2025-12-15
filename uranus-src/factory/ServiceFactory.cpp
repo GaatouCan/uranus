a516257771084bd6cc56b56953b847d237248b5b
admin <xxx@xx.xom>
2025-12-15 12:42:07 +0800

    Initial commit

--- uranus-src/factory/ServiceFactory.cpp
+++ uranus-src/factory/ServiceFactory.cpp
@@ -5,10 +5,20 @@ namespace uranus {
     }
 
     ServiceFactory::~ServiceFactory() {
+        services_.clear();
     }
 
     ServiceFactory &ServiceFactory::instance() {
         static ServiceFactory _inst;
         return _inst;
+    }
+
+    void ServiceFactory::initial() {
+    }
+
+    BaseService *ServiceFactory::create(const std::string &path) {
+    }
+
+    void ServiceFactory::destroy(BaseService *ptr, const std::string &path) {
     }
 } // uranus
--- uranus-src/factory/ServiceFactory.h
+++ uranus-src/factory/ServiceFactory.h
@@ -3,8 +3,18 @@
 #include <base/noncopy.h>
 #include <base/SharedLibrary.h>
 
+#include <unordered_map>
 
 namespace uranus {
+
+    namespace actor {
+        class BaseService;
+    }
+    using actor::BaseService;
+
+    using ServiceCreator = BaseService *(*)();
+    using ServiceDeleter = void (*)(BaseService *);
+
     class ServiceFactory final {
 
         ServiceFactory();
@@ -15,5 +25,19 @@ namespace uranus {
         DISABLE_COPY_MOVE(ServiceFactory)
 
         static ServiceFactory& instance();
+
+        void initial();
+
+        BaseService *create(const std::string &path);
+        void destroy(BaseService *ptr, const std::string &path);
+
+    private:
+        struct ServiceNode {
+            SharedLibrary lib;
+            ServiceCreator ctor = nullptr;
+            ServiceDeleter del = nullptr;
+        };
+
+        std::unordered_map<std::string, ServiceNode> services_;
     };
 } // uranus
