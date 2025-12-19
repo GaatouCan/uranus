ce5d0eee7d2911ca4f1930bf26e8f7de41f0c74f
astria <1752651025@qq.com>
2025-12-19 21:51:53 +0800

    Initial commit

---
+++ config-src/include/config/ConfigModule.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "config.export.h"
+#include <actor/ServerModule.h>
+
+
+namespace uranus::config {
+
+    using actor::ServerModule;
+
+    class CONFIG_API ConfigModule final : public ServerModule {
+
+    public:
+        ConfigModule();
+        ~ConfigModule() override;
+
+        SERVER_MODULE_NAME(ConfigModule);
+
+        void start() override;
+        void stop() override;
+    };
+} // config
+// uranus
---
+++ config-src/source/config/ConfigModule.cpp
@@ -0,0 +1,15 @@
+#include "ConfigModule.h"
+
+namespace uranus::config {
+    ConfigModule::ConfigModule() {
+    }
+
+    ConfigModule::~ConfigModule() {
+    }
+
+    void ConfigModule::start() {
+    }
+
+    void ConfigModule::stop() {
+    }
+}
