10d93868cb62bd4d111d9f8c328b6d85c248bf5a
admin <xxx@xx.xom>
2026-02-06 10:12:12 +0800

    Initial commit

--- gameplay/friend/FriendService.cpp
+++ gameplay/friend/FriendService.cpp
@@ -1,9 +1,16 @@
 #include "FriendService.h"
 #include "common/ProtocolID.h"
 
+#include <actor/ActorContext.h>
+#include <logger/LoggerModule.h>
+
 #include <greeting.pb.h>
 
+
 namespace gameplay {
+
+    using uranus::logger::LoggerModule;
+
     FriendService::FriendService() {
         enableTick_ = true;
     }
@@ -15,16 +22,28 @@ namespace gameplay {
         return "FriendService";
     }
 
+    void FriendService::onInitial(ActorContext *ctx) {
+        super::onInitial(ctx);
+
+        if (auto *module = ACTOR_GET_MODULE(LoggerModule)) {
+            module->createLogger("friend_service", "friend");
+        }
+    }
+
     void FriendService::onPackage(int64_t src, PackageHandle &&pkg) {
         using namespace protocol;
 
         if (pkg == nullptr)
             return;
 
+        auto logger = spdlog::get("friend_service");
+
         switch (pkg->id_) {
             case kSyncPlayerInfo: {
                 greeting::SyncPlayerInfo info;
                 info.ParseFromArray(pkg->payload_.data(), pkg->payload_.size());
+
+                logger->info("Sync player[{}] info", src);
             }
             break;
             default: break;
--- gameplay/friend/FriendService.h
+++ gameplay/friend/FriendService.h
@@ -7,14 +7,19 @@ namespace gameplay {
     using uranus::actor::BaseService;
     using uranus::actor::PackageHandle;
     using uranus::actor::DataAsset;
+    using uranus::actor::ActorContext;
 
     class FriendService final : public BaseService {
+
+        using super = BaseService;
 
     public:
         FriendService();
         ~FriendService() override;
 
         [[nodiscard]] std::string getName() const override;
+
+        void onInitial(ActorContext *ctx) override;
 
         void onPackage(int64_t src, PackageHandle &&pkg) override;
         void onEvent(int64_t evt, DataAsset *data) override;
